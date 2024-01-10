8d34362d276a622057df1330db41e2ff43955807
Daniel Herrera <dannyherrea97@gmail.com>
2024-01-10 17:57:47 -0600

    404

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 class Config():
-
+    
     FLASK_APP = os.getenv('FLASK_APP')
     FLASK_ENV = os.getenv('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY')
